f5c36d3ab0dff840c74e2502eb24469f7e289283
SungjinKim <a01081440011@gmail.com>
2022-02-11 15:34:43 +0900

    Time: 162 ms (32.08%), Space: 44.8 MB (16.18%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.js
+++ 455-assign-cookies/455-assign-cookies.js
@@ -7,13 +7,14 @@ var findContentChildren = function(g, s) {
   const cookies = s.sort((a, b) => a - b);
   const children = g.sort((a, b) => a - b);
   let result = 0;
+  let lastIndex = 0;
   
   cookies.forEach((cookie) => {
-    for (let i = 0; i < children.length; i++) {
+    for (let i = lastIndex; i < children.length; i++) {
       if (children[i] > cookie) continue;
 
-      children.splice(i, 1);
       result++;
+      lastIndex = i + 1;
       return;
     }
   });
