fcfe2110c99d32871a473d27314f994145b97d43
SungjinKim <a01081440011@gmail.com>
2022-02-16 18:53:38 +0900

    Update 338-counting-bits.js

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -4,33 +4,18 @@
  */
 var countBits = function(n) {
   const result = [0];
-  const dp = [];
-  let addMode = false;
-  let dpIdx = 0;
+  let currIdx = 0;
+  let endIdx = 0;
   
-  for (let i = 1; i < n + 1; i++) {
-    if (!dp.length) {
-      result.push(1);
-      dp.push(1);
-      continue;
-    }
+  for (let i = 0; i < n; i++) {
+    result.push(result[currIdx] + 1);
     
-    if (dpIdx >= dp.length) {
-      dpIdx = 0;
-      addMode = !addMode;
-      
-      if (!addMode) {
-        dp.push(...result.slice(-dp.length));
-      }
-    }
-    
-    if (addMode) {
-      result.push(dp[dpIdx] + 1);
+    if (currIdx < endIdx) {
+      currIdx++;
     } else {
-      result.push(dp[dpIdx]);
+      currIdx = 0;
+      endIdx = result.length - 1;
     }
-    
-    dpIdx++;
   }
   
   return result;
