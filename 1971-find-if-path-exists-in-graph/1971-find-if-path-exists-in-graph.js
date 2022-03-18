44509e52c1af872ac027ae4b6d85d2d6f731e517
SungjinKim <a01081440011@gmail.com>
2022-03-18 15:40:01 +0900

    Time: 764 ms (51.31%), Space: 144.6 MB (68.95%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+  if (!edges.length) return true;
+  
+  const vertices = {};
+  
+  edges.forEach(([from, to]) => {
+    if (!vertices[from]) {
+      vertices[from] = new Vertex(from);
+    }
+    
+    if (!vertices[to]) {
+      vertices[to] = new Vertex(to);
+    }
+    
+    vertices[from].addEdge(vertices[to]);
+    vertices[to].addEdge(vertices[from]);
+  });
+  
+  
+  const findVertex = (sourceVertex, destination) => {
+    if (sourceVertex.isChecked) {
+      return false;
+    }
+    
+    if (sourceVertex.value === destination) {
+       return true;
+    }
+    
+    sourceVertex.isChecked = true;
+    return sourceVertex.edge.some((vertex) => findVertex(vertex, destination));
+  }
+  
+  return findVertex(vertices[source], destination);
+};
+
+class Vertex {
+  constructor(value) {
+    this.value = value;
+    this.isChecked = false;
+    this.edge = [];
+  }
+  
+  addEdge(vertex) {
+    this.edge.push(vertex);
+  }
+}
